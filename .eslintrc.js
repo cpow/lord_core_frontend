bf89f2b266bd441961d2b81922616dd3fe208742
cpow <cpow85@gmail.com>
2017-03-19 17:42:32 -0400

    init ember application

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  extends: 'eslint:recommended',
+  env: {
+    browser: true
+  },
+AWS_REGION=eu-west-2
+AWS_BUCKET=gameface-united-kingdom
+  rules: {
+  }
+};
