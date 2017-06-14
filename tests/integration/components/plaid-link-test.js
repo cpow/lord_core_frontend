e29c251f7d7b1098436e6b2fec861e76fd696c59
cpow <cpow85@gmail.com>
2017-06-14 11:17:38 -0400

    fixes: https://github.com/cpow/property-management/issues/3

--- tests/integration/components/plaid-link-test.js
+++ tests/integration/components/plaid-link-test.js
@@ -13,15 +13,6 @@ describe('Integration | Component | plaid link', function() {
     integration: true
   });
   it('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#plaid-link}}
-    //     template content
-    //   {{/plaid-link}}
-    // `);
-
     this.render(hbs`{{plaid-link}}`);
     expect(this.$()).to.have.length(1);
   });
