44372ca5f63d79ad03ef64723c2d06eea64d2a18
cpow <cpow85@gmail.com>
2017-07-06 11:16:46 -0400

    refactor saveTask to the component rendering it

--- app/components/properties/new-form.js
+++ app/components/properties/new-form.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
 
-export default Ember.Component.extend({
+const { Component, get } = Ember;
+
+export default Component.extend({
+  saveTask: task(function * (changeset) {
+    changeset.validate();
+    if (changeset.isValid) {
+      try{
+        yield changeset.save();
+        this.transitionToRoute('properties');
+      } catch(e) {
+        let errors = get(this, 'model.errors');
+        errors.forEach(error => {
+          let key = error.attribute;
+          let validation = error.message;
+          changeset.addError(key, { validation });
+        });
+      }
+    }
+  }),
 });
--- app/controllers/properties/new.js
+++ app/controllers/properties/new.js
@@ -1,26 +1,8 @@
 import Ember from 'ember';
-import { task } from 'ember-concurrency';
 import PropertyValidation from 'lord-core-frontend/validations/property';
 
-const { Controller, get } = Ember;
+const { Controller } = Ember;
 
 export default Controller.extend({
   PropertyValidation,
-
-  saveProperty: task(function * (changeset) {
-    changeset.validate();
-    if (changeset.isValid) {
-      try{
-        yield changeset.save();
-        this.transitionToRoute('properties');
-      } catch(e) {
-        let errors = get(this, 'model.errors');
-        errors.forEach(error => {
-          let key = error.attribute;
-          let validation = error.message;
-          changeset.addError(key, { validation });
-        });
-      }
-    }
-  }),
 });
